4df1bb8294a5e6904309a2c4288bd6f980c44110
mukul550s <skmukulmia39@gmail.com>
2026-08-01 21:54:35 +0600

    Add meme command to send funny images

---
+++ Script/commands/memes.js
@@ -0,0 +1,27 @@
+const fs = require("fs-extra");
+
+module.exports.config = {
+  name: "meme",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Mukul",
+  description: "Funny meme",
+  commandCategory: "fun",
+  cooldowns: 5
+};
+
+module.exports.run = async ({ api, event }) => {
+  const imgPath = __dirname + "/cache/meme.jpg"; // এখানে আপনার ছবিটি রাখুন
+
+  if (!fs.existsSync(imgPath))
+    return api.sendMessage("https://i.imgur.com/zCFuB2S.jpeg", event.threadID);
+
+  api.sendMessage(
+    {
+      body: "😂 অনেক দিন বিড়ি খাইনা, ১০ টাকা দাও বাবা...🙂",
+      attachment: fs.createReadStream(imgPath)
+    },
+    event.threadID,
+    event.messageID
+  );
+};
